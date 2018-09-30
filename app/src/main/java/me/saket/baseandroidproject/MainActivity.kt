06da25cfec1cee6c3b849f52d8284f2d4adc34e6
Saket Narayan <saket@uncommon.is>
2018-09-30 20:24:51 +0530

    Animate entry and exit of keyboard

--- app/src/main/java/me/saket/baseandroidproject/MainActivity.kt
+++ app/src/main/java/me/saket/baseandroidproject/MainActivity.kt
@@ -1,14 +1,81 @@
 package me.saket.baseandroidproject
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
+import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.ViewGroup
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.subjects.PublishSubject
+import kotterknife.bindView
 
+@SuppressLint("CheckResult")
 class MainActivity : AppCompatActivity() {
+
+  private val contentView by bindView<View>(R.id.contentview)
+  private val keyboardView by bindView<View>(R.id.keyboard)
+
+  private val onDestroy = PublishSubject.create<Any>()
+  private val smoothly = true
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    // TODO: Dagger?
+    RxView.clicks(contentView)
+        .observeOn(mainThread())
+        .takeUntil(onDestroy)
+        .scan(false) { keyboardVisible, _ -> keyboardVisible.not() }
+        .subscribe { show ->
+          if (show) {
+            showKeyboard()
+          } else {
+            hideKeyboard()
+          }
+        }
+  }
+
+  override fun onDestroy() {
+    onDestroy.onNext(Any())
+    super.onDestroy()
+  }
+
+  private fun showKeyboard() {
+    val params = contentView.layoutParams as ViewGroup.MarginLayoutParams
+    params.bottomMargin = 150.dp
+    
+    keyboardView.visibility = View.VISIBLE
+    keyboardView.alpha = 0F
+    keyboardView.onNextMeasure {
+      keyboardView.translationY = keyboardView.height / 4F
+    }
+
+    keyboardView.animate()
+        .alpha(1F)
+        .translationY(0F)
+        .setInterpolator(FastOutSlowInInterpolator())
+        .start()
+  }
+
+  private fun hideKeyboard() {
+    val params = contentView.layoutParams as ViewGroup.MarginLayoutParams
+    params.bottomMargin = 24.dp
+
+    keyboardView.animate()
+        .alpha(0F)
+        .translationY(keyboardView.height / 4F)
+        .setInterpolator(FastOutSlowInInterpolator())
+        .withEndAction { keyboardView.visibility = View.GONE }
+        .start()
   }
 }
+
+private val Number.dp: Int
+  get() {
+    val metrics = Resources.getSystem().displayMetrics
+    return (this.toInt() * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
+  }
---
+++ app/src/main/java/me/saket/baseandroidproject/Views.kt
@@ -0,0 +1,41 @@
+package me.saket.baseandroidproject
+
+import android.content.res.Resources
+import android.view.View
+import android.view.ViewTreeObserver
+import timber.log.Timber
+
+/**
+ * Run a function when a View gets measured and laid out on the screen.
+ */
+fun View.onNextMeasure(runnable: () -> Unit) {
+  if (isInEditMode || isLaidOut) {
+    runnable()
+    return
+  }
+
+  viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+    override fun onPreDraw(): Boolean {
+      if (isLaidOut) {
+        viewTreeObserver.removeOnPreDrawListener(this)
+        runnable()
+
+      } else if (visibility == View.GONE) {
+        Timber.w("View's visibility is set to Gone. It'll never be measured: %s", resourceName())
+        viewTreeObserver.removeOnPreDrawListener(this)
+      }
+      return true
+    }
+  })
+}
+
+fun View.resourceName(): String {
+  var name = "<nameless>"
+  try {
+    name = resources.getResourceEntryName(id)
+  } catch (e: Resources.NotFoundException) {
+    // Nothing to see here
+  }
+  return name
+}
+
