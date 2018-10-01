c464384bf4920c7c1553e09313f12e698e8aee87
Saket Narayan <saket@uncommon.is>
2018-10-02 01:36:02 +0530

    Move ActivityViewHolder to a separate file

--- fluidresizelayout/src/main/java/me/saket/fluidresize/KeyboardVisibilityDetector.kt
+++ fluidresizelayout/src/main/java/me/saket/fluidresize/KeyboardVisibilityDetector.kt
@@ -1,7 +1,5 @@
 package me.saket.fluidresize
 
-import android.view.View
-import android.view.ViewGroup
 import android.view.ViewTreeObserver
 
 object KeyboardVisibilityDetector {
@@ -51,19 +49,3 @@ object KeyboardVisibilityDetector {
   }
 }
 
-data class ActivityViewHolder(
-    val decorView: ViewGroup,
-    val contentViewFrame: ViewGroup,
-    val contentView: ViewGroup
-) {
-
-  fun onDetach(onDetach: () -> Unit) {
-    decorView.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
-      override fun onViewDetachedFromWindow(v: View?) {
-        onDetach()
-      }
-
-      override fun onViewAttachedToWindow(v: View?) {}
-    })
-  }
-}
